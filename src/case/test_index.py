175fc16fe08358e547526cd6d6d11eb5c1a9df0c
addoneyy <addone911@gmail.com>
2026-02-10 15:51:35 +0800

    update conf

--- src/case/test_index.py
+++ src/case/test_index.py
@@ -2,13 +2,13 @@ import requests
 from  jsonpath import jsonpath
 
 class TestIndex:
-    def test_index_list(self):
+    def test_index_list(self, get_token):
         """
             获取列表
         :return:
         """
         url = "http://www.xxx/api/login"
-        header = {"token": "jw8d1i903whjcdf0193jfdci8h23989"}
+        header = {"token": get_token}
         data = {"type": "test"}
         resp = requests.request(method="post", url=url, json=data, headers=header)
         assert resp.status_code == 200
