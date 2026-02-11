bdfd666f2e410b20056e6924c62ee06ea11b822a
addoneyy <addone911@gmail.com>
2026-02-11 15:44:45 +0800

    update single api test

--- src/case/test_index.py
+++ src/case/test_index.py
@@ -1,16 +1,30 @@
+import  pytest
 import requests
 from  jsonpath import jsonpath
 
 class TestIndex:
+
+    @pytest.mark.skip(reason = "skip")
     def test_index_list(self, get_token):
         """
             获取列表
         :return:
         """
-        url = "http://www.xxx/api/login"
+        url = "http://www.xxx/api/tasklist"
         header = {"token": get_token}
         data = {"type": "test"}
         resp = requests.request(method="post", url=url, json=data, headers=header)
         assert resp.status_code == 200
-        assert jsonpath(resp.json(), "$.msg")[0] == "列表获取成功！"
+        assert jsonpath(resp.json(), "$..msg")[0] == "列表获取成功！"
 
+    def test_index_list_token_error(self):
+        """
+            错误token获取列表失败
+        :return:
+        """
+        url = "http://www.xxx/api/tasklist"
+        header = {"token": "1234567"}
+        data = {"type": "test"}
+        resp = requests.request(method="post", url=url, json=data, headers=header)
+        assert resp.status_code == 200
+        assert jsonpath(resp.json(), "$..msg")[0] == "列表获取失败！"
--- src/case/test_login.py
+++ src/case/test_login.py
@@ -1,8 +1,11 @@
+import  pytest
 import  requests
 from jsonpath import jsonpath
 
 class TestLogin:
-    def test_login_success_01(self):
+
+    @pytest.mark.skip(reason = "skip")
+    def test_login_success(self):
         """"
             用户登录成功
         """
@@ -11,3 +14,23 @@ class TestLogin:
         resp = requests.request(method="post", url=url, json=data)
         assert resp.status_code == 200
         assert jsonpath(resp.json(),"$..msg")[0] == "登录成功"
+
+    def test_login_fail_phone_error(self):
+        """"
+            用户手机号错误登录失败
+        """
+        url = "http://www.xxx/api/login"
+        data = {"phone": "182123412345", "code": "123456"}
+        resp = requests.request(method="post", url=url, json=data)
+        assert resp.status_code == 200
+        assert jsonpath(resp.json(), "$..msg")[0] == "登录失败"
+
+    def test_login_fail_code_error(self):
+        """"
+            用户验证码错误登录失败
+        """
+        url = "http://www.xxx/api/login"
+        data = {"phone": "18212341234", "code": "111222"}
+        resp = requests.request(method="post", url=url, json=data)
+        assert resp.status_code == 200
+        assert jsonpath(resp.json(), "$..msg")[0] == "验证码不正确"
