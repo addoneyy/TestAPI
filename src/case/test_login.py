9dd9f7fcfcd1e1f80113115fa6ae3f01e798a949
addoneyy <addone911@gmail.com>
2026-02-27 14:21:51 +0800

    update datadrive

--- src/case/test_login.py
+++ src/case/test_login.py
@@ -1,36 +1,14 @@
 import  pytest
 import  requests
 from jsonpath import jsonpath
+from src.utils.yamlloader import YamlLoader
+from src.utils.validator import Validator
 
 class TestLogin:
 
-    @pytest.mark.skip(reason = "skip")
-    def test_login_success(self):
-        """"
-            用户登录成功
-        """
-        url = "http://www.xxx/api/login"
-        data = {"phone": "18212341234", "code": "123456"}
-        resp = requests.request(method="post", url=url, json=data)
-        assert resp.status_code == 200
-        assert jsonpath(resp.json(),"$..msg")[0] == "登录成功"
-
-    def test_login_fail_phone_error(self):
-        """"
-            用户手机号错误登录失败
-        """
-        url = "http://www.xxx/api/login"
-        data = {"phone": "182123412345", "code": "123456"}
-        resp = requests.request(method="post", url=url, json=data)
-        assert resp.status_code == 200
-        assert jsonpath(resp.json(), "$..msg")[0] == "登录失败"
-
-    def test_login_fail_code_error(self):
-        """"
-            用户验证码错误登录失败
-        """
-        url = "http://www.xxx/api/login"
-        data = {"phone": "18212341234", "code": "111222"}
-        resp = requests.request(method="post", url=url, json=data)
-        assert resp.status_code == 200
-        assert jsonpath(resp.json(), "$..msg")[0] == "验证码不正确"
+    datas = YamlLoader().load(r"data/test_login.yaml")["login"]
+    @pytest.mark.parametrize("data", datas)
+    def test_login(self, data):
+        resp = requests.request(**data["request"])
+        assert Validator().validate(data, resp)
+        print(f"测试用例[{data['name']}]执行成功")
