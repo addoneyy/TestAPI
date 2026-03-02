a1f39e08b97f9f3688eb4e805a007d0c4908fbed
addoneyy <addone911@gmail.com>
2026-03-02 14:44:17 +0800

    update case_process

---
+++ src/case/test_task.py
@@ -0,0 +1,67 @@
+import requests
+from jsonpath import jsonpath as jp
+
+class TestTask:
+
+
+    def __get_task_type(self, token):
+        """
+            获取任务
+        """
+        url = "http://127.0.0.1:xxxx/task/get/task"
+        headers = {"token": token}
+        resp = requests.request("get", url=url, headers=headers)
+        assert resp.status_code == 200
+        assert len(jp(resp.json(), '$.data')[0]) == 2
+
+        return True
+
+    def __upload_imgs(self, token):
+        """
+            上传图片
+        """
+        url = "http://127.0.0.1:xxxx/task/upload/task"
+        headers = {"token": token}
+        image = [("file", ('IMG_5977.JPG', open(r"data/imgs/IMG_5977.JPG"), 'image/jpeg'))]
+        resp = requests.request("post", url=url, data=image, headers=headers)
+        assert resp.status_code == 200
+        image_name = jp(resp.json(),'$.data')[0]
+
+        return True, image_name
+
+    def __submit_task(self, token, image_name):
+        """
+            提交任务
+        """
+        url = "http://127.0.0.1:xxxx/task/submit/task"
+        headers = {"token": token}
+        json = {"title": "测试任务", "content": "123123123", "type": 42, "imglist": [image_name],
+                "start_time": "2024-07-19 17:12"}
+        resp = requests.request("post", url=url, json=json, headers=headers)
+        assert resp.status_code == 200
+        assert jp(resp.json(), '$.msg')[0] == "操作成功！"
+        psid = jp(resp.json(), '$..id')[0]
+
+        return True, psid
+
+    def __pay_order(self, token="", psid="", payps="123456"):
+        """
+            支付订单
+        """
+        url = "http://127.0.0.1:xxxx/task/pay/task"
+        json = {"id": psid, "payps": payps}
+        headers = {"token": token}
+        resp = requests.request("post", url=url, json=json, headers=headers)
+        assert resp.status_code == 200
+
+        return True
+
+    def create_task(self, get_token):
+        """
+            创建任务业务流用例
+        """
+        self.token = get_token
+        assert self.__get_task_type()
+        assert self.__upload_imgs()
+        assert self.__submit_task()
+        assert self.__pay_order()
