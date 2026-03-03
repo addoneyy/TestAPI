045cdcd621cd64fb8f5a4770718b5f3400777210
addoneyy <addone911@gmail.com>
2026-03-03 11:00:29 +0800

    update import

--- src/utils/tagfunc.py
+++ src/utils/tagfunc.py
@@ -7,6 +7,8 @@ def get_user_token(loader=None, node=None):
     """
         获取token的pyyaml自定义标签
     """
+    #解决循环导入
+    from src.utils.yamlloader import YamlLoader
     INIT_DATA = YamlLoader().load(r"data/init_data.yaml")["get_user_token"]
     url = "http://www.xxx/api/login"
     resp = http_request.request(url=url, method="post", json=INIT_DATA)
