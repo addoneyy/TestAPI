51250b01974d31917d3b9eb4128c0522e8f8ab54
addoneyy <addone911@gmail.com>
2026-03-03 15:15:19 +0800

    update send_mail_result

--- src/conftest.py
+++ src/conftest.py
@@ -1,12 +1,18 @@
 import os
 import sys
+
+from src.utils.mail import Mail
+
 sys.path.append(os.getcwd())
+from datetime import datetime
+sys.path.append(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
 
 import pytest
 import time
 from src.utils.tagfunc import get_user_token
 from src.utils.mysql import MysqlClient
 from src.utils.report import Report
+
 
 
 @pytest.fixture(scope='function')
@@ -50,8 +56,23 @@ def create_task():
     res = MysqlClient().query(sql)
     yield res[0][0]
 
+
+def pytest_config(config):
+    """
+        产生全局变量的配置函数
+    """
+    item = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    sys.path.append(item)
+
+
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     """
         获取所有的测试用例结果
     """
     passed, failed, skipped = Report().get_report_summary(terminalreporter)
+    subject = f"自动化测试报告{sys.path[-1]}"
+    content = f"测试结果如下:\n测试通过用例：\n    {len(passed)}个用例通过\n"
+    content = content + f"\n测试跳过用例：\n    {len(skipped)}个用例跳过\n"
+    content = content + f"\n测试失败用例：\n    {len(failed)}个用例执行失败\n"
+    Mail().send_mail(subject, content)
+
